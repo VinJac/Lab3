0c41d5e8474309f1b1e021607283a0c2f0abaf42
Alexandre <bocketo@gmail.com>
2018-01-22 15:53:06 +0100

    début userlist

--- src/model/UserList.java
+++ src/model/UserList.java
@@ -1,6 +1,17 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserList {
+	
+	private List<String> userList;
+	
+	private UserList(){
+		userList = new ArrayList<>();
+	}
+	
+	boolean addUser(String user){
+		
+	}
 }
