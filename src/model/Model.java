ec06340d514ba13b3778c127c4fbd324fecef4e8
Benoit Chaurand <benoitrire@hotmail.fr>
2018-01-22 16:15:51 +0100

    Exo 1

---
+++ src/model/Model.java
@@ -0,0 +1,5 @@
+package model;
+
+public interface Model{
+    String[] getData(); //return the userList for the view to avoif spagetti code
+}
--- src/model/UserList.java
+++ src/model/UserList.java
@@ -3,7 +3,7 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserList {
+public class UserList implements Model{
 	
 	private List<String> userList;
 	
@@ -12,6 +12,12 @@ public class UserList {
 	}
 	
 	boolean addUser(String user){
-		
+		return userList.add(user); //add a user and return true if succeed, false instead
 	}
+
+    @Override
+    public String[] getData() {
+        return userList.toArray(new String[]{}); //return a string[]
+    }
+        
 }
